8624d0bdbe2d25796a5629aa4574d638c723b420
Kareem Mohamed <kareem_basha61@yahoo.com>
2021-02-02 23:06:56 +0200

    fix

--- assets/mathquill/english/init.js
+++ assets/mathquill/english/init.js
@@ -1,1 +1,8 @@
-
+function initQuill(){
+    if(typeof(MathQuill) === undefined){
+        window.setTimeout(initQuill,500);
+        return;
+    }
+    var MQ = MathQuill.getInterface(2);
+}
+initQuill();
