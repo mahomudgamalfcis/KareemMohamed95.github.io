facaf52ee7e29c583cb4915de25de0bee385a04c
Kareem Mohamed <kareem_basha61@yahoo.com>
2021-03-02 20:06:38 +0200

    Update algebrite-browser.js

--- math-assets/algebrite/algebrite.js
+++ math-assets/algebrite/algebrite.js
@@ -1,26 +1,48 @@
-run = function(math, return_input_id){
-    var Algebrite = require('algebrite');
-    var ans = Algebrite.run(math);
-	var ansinfo = {result: ans, nonce: Math.random()}
-	Shiny.onInputChange(return_input_id, ansinfo );
+run = function (math, return_input_id) {
+  var Algebrite = require('algebrite');
+  var ans = Algebrite.run(math);
+  var ansinfo = { result: ans, nonce: Math.random() }
+  Shiny.onInputChange(return_input_id, ansinfo);
 }
-simplify = function(math, return_input_id){
-    var Algebrite = require('algebrite');
-    var ans = Algebrite.simplify(math).toString();
-	var ansinfo = {result: ans, nonce: Math.random()}
-	Shiny.onInputChange(return_input_id, ansinfo );
+simplify = function (math, return_input_id) {
+  var Algebrite = require('algebrite');
+  var ans = Algebrite.simplify(math).toString();
+  var ansinfo = { result: ans, nonce: Math.random() }
+  Shiny.onInputChange(return_input_id, ansinfo);
 }
-diffrentiate = function(math, degree, return_input_id){
-    var Algebrite = require('algebrite');
-    var simpleMath = Algebrite.simplify(math).toString();
-	var ans = Algebrite.derivative(simpleMath, degree).toString();
-	var ansinfo = {result: ans, nonce: Math.random()}
-	Shiny.onInputChange(return_input_id, ansinfo );
+simplify_list = function (math_list, return_input_id) {
+  var Algebrite = require('algebrite');
+  math_list_array = math_list.split(" ");
+  var ansinfo = { nonce: Math.random() }
+  var ans;
+  for (var i = 0; i < math_list_array.length; i++) {
+    ans = Algebrite.simplify(math_list_array[i]).toString();
+    ansinfo[["result" + ((i + 1).toString())]] = ans;
+  }
+  Shiny.onInputChange(return_input_id, ansinfo);
 }
-integrate = function(math, degree, return_input_id){
-    var Algebrite = require('algebrite');
-    var simpleMath = Algebrite.simplify(math).toString();
-	var ans = Algebrite.integral(simpleMath, degree).toString();
-	var ansinfo = {result: ans, nonce: Math.random()}
-	Shiny.onInputChange(return_input_id, ansinfo );
+diffrentiate = function (math, degree, return_input_id) {
+  var Algebrite = require('algebrite');
+  var simpleMath = Algebrite.simplify(math).toString();
+  var ans = Algebrite.derivative(simpleMath, degree).toString();
+  var ansinfo = { result: ans, nonce: Math.random() }
+  Shiny.onInputChange(return_input_id, ansinfo);
+}
+diffrentiate_list = function (math_list, degree, return_input_id) {
+  var Algebrite = require('algebrite');
+  math_list_array = math_list.split(" ");
+  var ansinfo = { nonce: Math.random() }
+  var ans;
+  for (var i = 0; i < math_list_array.length; i++) {
+    ans = Algebrite.derivative(math_list_array[i], degree).toString();
+    ansinfo[["result" + ((i + 1).toString())]] = ans;
+  }
+  Shiny.onInputChange(return_input_id, ansinfo);
+}
+integrate = function (math, degree, return_input_id) {
+  var Algebrite = require('algebrite');
+  var simpleMath = Algebrite.simplify(math).toString();
+  var ans = Algebrite.integral(simpleMath, degree).toString();
+  var ansinfo = { result: ans, nonce: Math.random() }
+  Shiny.onInputChange(return_input_id, ansinfo);
 }
